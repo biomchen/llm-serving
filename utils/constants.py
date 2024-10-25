9042800d7e41ea292d364dc501d4e4d10bae4404
biomchen <meng.chen03@gmail.com>
2024-10-24 21:02:44 -0400

    updated model path in the constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -3,7 +3,7 @@ CONFIGS_PATH = 'configs'
 LLM_CONFIGS = 'configs-llm.json'
 
 # LLM path
-MODEL_PATH = '/home/cm/models'
+MODEL_PATH = '{YOU_MODEL_PATH}'
 
 # model api
 MODEL_API = "http://localhost:8000/model"
